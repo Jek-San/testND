287d43966e6a007c046c9e3d4334ebabd0051819
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-02-20 04:58:35 +0700

    Model-View-ViewModel Architecture for Xamarin.Forms

---
+++ TestND/TestND/ViewModels/CoffeEquipmentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TestND.ViewModels
+{
+    public class CoffeEquipmentViewModel : BindableObject
+    {
+        public CoffeEquipmentViewModel() { 
+			IncreaseCount = new Command(OnIncrease);
+
+        }
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+        int count = 0;
+
+        public ICommand IncreaseCount { get; }
+
+
+        string countDisplay = "Click Me";
+
+
+        public string CountDisplay
+        {
+            get => countDisplay; set
+            {
+                if (value == countDisplay)
+                {
+                    return;
+                }
+                else
+                {
+                    countDisplay = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
