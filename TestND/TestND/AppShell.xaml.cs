a330caf54e652b6eba2c2726201ac3e651c8b894
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-02-20 03:38:39 +0700

    BlankkAppXamarin

---
+++ TestND/TestND/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TestND
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+           
+        }
+
+       
+    }
+}
